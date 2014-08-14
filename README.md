374f11cba68220f41e0d5701ab0f1f4dfd85fab3
awong <alan_kt_wong@yahoo.com>
2014-08-13 20:47:17 -0400

    Simple play

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 play-eventsourced
 =================
+
+This is planned to be a [cqrs](http://martinfowler.com/bliki/CQRS.html)/[event-sourced](http://martinfowler.com/eaaDev/EventSourcing.html) example, which is inspired by:
+
+1. Primarily by [Erik Rozendaal's simple example with a blog in Play](http://blog.zilverline.com/2012/07/04/simple-event-sourcing-introduction-part-1/)
+2. [his example with an invoice](https://github.com/erikrozendaal/scala-event-sourcing-example)
+3. [Martin Krassner's example](https://github.com/eligosource/eventsourced-example) 
+4. [Pawel Kaczor's example using Akka](http://pkaczor.blogspot.com/2014/04/reactive-ddd-with-akka.html)
+5. a [real-world example](http://www.parleys.com/play/53a7d2cce4b0543940d9e55c)
+6. the [cqrs journey taken by folks at MSDN](http://msdn.microsoft.com/en-us/library/jj554200.aspx)
+
+
+Technologies Used
+-----------------
+
+Technologies used in this repo are:
+
+1. Java JDK 1.7.0_51
+2. [Scala](http://www.scala-lang.org/) 2.10.4
+3. [SBT](http://www.scala-sbt.org/index.html) 0.12.2
+4. [Akka](http://akka.io/) [2.3.4](http://doc.akka.io/docs/akka/2.3.4/scala.html) ... especially [Akka Persistence](http://doc.akka.io/docs/akka/2.3.4/scala/persistence.html) which replaces the [event sourced library](https://github.com/eligosource/eventsourced).
+5. [Play](http://www.playframework.com/) 2.1.5
+6. [Redis](http://redis.io/) 2.8.13 is what I know works on my machine
+
---
+++ project/Build.scala
@@ -0,0 +1,112 @@
+import sbt._
+import Keys._
+import play.Project._
+
+import sbt._
+import Keys._
+import play.Project._
+
+object Resolvers {
+  lazy val nexusSnapshots        = "Sonatype Nexus Repository Manager Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+  lazy val sprayIoReleases       = "Spray IO Release Repo" at "http://repo.spray.io"
+  lazy val typesafeReleases      = "Typesafe Releases Repository"  at "http://repo.typesafe.com/typesafe/releases/"
+  lazy val typesafeSnapshots     = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
+  lazy val typesafeResolvers     = Seq(typesafeReleases, typesafeSnapshots, sprayIoReleases, nexusSnapshots)
+
+  lazy val sunRepo               = "Sun Maven2 Repo"               at "http://download.java.net/maven/2"
+  lazy val glassfishRepo         = "Sun GF Maven2 Repo"            at "http://download.java.net/maven/glassfish"
+  lazy val oracleRepo            = "Oracle Maven2 Repo"            at "http://download.oracle.com/maven"
+  lazy val oracleResolvers       = Seq(sunRepo, oracleRepo)
+
+  lazy val springRelease         = "EBR Spring Release Repository" at "http://repository.springsource.com/maven/bundles/release"
+  lazy val springExternalRelease = "EBR Spring External Release"   at "http://repository.springsource.com/maven/bundles/external"
+  lazy val springMilestoneRepo   = "Spring Milestone Repository"   at "https://repo.springsource.org/libs-milestone"
+  lazy val springAppResolvers    = Seq(springRelease, springExternalRelease, springMilestoneRepo)
+
+  lazy val jBossRepo             = "JBoss Public Maven Repo"       at "https://repository.jboss.org/nexus/content/groups/public-jboss/"
+
+  lazy val allResolvers          = typesafeResolvers ++ springAppResolvers ++ oracleResolvers ++ Seq(jBossRepo)
+}
+
+object Versions {
+  lazy val scalaVer  = "2.10.4"
+  lazy val akkaVer   = "2.1.0"
+  lazy val springVer = "3.1.2.RELEASE"
+
+  lazy val playVer       = play.core.PlayVersion.current
+  lazy val junitVer      = "4.10"
+  lazy val junitVer      = "4.10"
+  lazy val scalaTestVer  = "2.1.0"
+  lazy val scalaCheckVer = "1.10.1"
+  lazy val scalazVer     = "7.0.0"
+  lazy val scalazVer     = "7.0.0"
+  lazy val scalaStmVer   = "0.6"
+
+}
+
+// Add your project dependencies here,
+object Dependencies {
+  import Versions._
+  
+  lazy val provided = "provided"
+  lazy val test     = "test"
+  lazy val runtime  = "runtime"
+
+  lazy val junit                = "junit"            %  "junit"           % junitVer      % test
+  lazy val mockito              = "org.mockito"      %  "mockito-core"    % "1.9.5"       % test
+  lazy val scalaCheck           = "org.scalacheck"   %% "scalacheck"      % scalaCheckVer % test
+  lazy val scalaTest            = "org.scalatest"    %% "scalatest"       % scalaTestVer  % test
+  lazy val testDependencies     = Seq(junit, scalaTest, scalaCheck, mockito)
+
+  lazy val akkaOrg              = "com.typesafe.akka"
+  lazy val akkaPersistArt       = "akka-persistence-experimental"
+
+  lazy val akkaActor            = akkaOrg            %% "akka-actor"      % akkaVer
+  lazy val akkaTestkit          = akkaOrg            %% "akka-testkit"    % akkaVer  % test
+  lazy val akkaRemote           = akkaOrg            %% "akka-remote"     % akkaVer
+  lazy val akkaSlf4j            = akkaOrg            %% "akka-slf4j"      % akkaVer
+  lazy val akkaCamel            = akkaOrg            %% "akka-camel"      % akkaVer
+  lazy val akkaPersistence      = akkaOrg            %% akkaPersistArt    % akkaVer
+  
+  lazy val akkaDependencies     = Seq(akkaActor, akkaTestkit, akkaRemote, akkaSlf4j, akkaCamel)
+
+  lazy val springOrg            = "org.springframework"
+  lazy val springScalaOrg       = springOrg + ".scala"
+  lazy val javaxInject          = "javax.inject"      % "javax.inject"                    % "1"
+  lazy val springAsm            = springOrg           % "org.springframework.asm"         % springVer
+  lazy val springAop            = springOrg           % "org.springframework.aop"         % springVer
+  lazy val springBeans          = springOrg           % "org.springframework.beans"       % springVer
+  lazy val springCore           = springOrg           % "org.springframework.core"        % springVer
+  lazy val springContext        = springOrg           % "org.springframework.context"     % springVer
+  lazy val springExpression     = springOrg           % "org.springframework.expression"  % springVer
+  lazy val springTxn            = springOrg           % "org.springframework.transaction" % springVer
+  lazy val springOrm            = springOrg           % "org.springframework.orm"         % springVer
+  lazy val springScala          = springScalaOrg      % "spring-scala"                    % "1.0.0.M2"
+  
+  lazy val springDependencies   = Seq(javaxInject, springBeans, springCore, springContext, springScala)
+
+  lazy val rxScala        = "com.netflix.rxjava"     %  "rxjava-scala"   % "0.14.6"
+  lazy val scalaAsync     = "org.scala-lang.modules" %% "scala-async"    % "0.9.0-M2"
+  lazy val squeryl        = "org.squeryl"            %% "squeryl"        % "0.9.5-6"
+  lazy val utilDependencies = Seq(rxScala, scalaAsync, squeryl)
+
+  lazy val basicDependencies    = testDependencies ++ springDependencies
+}
+
+object ApplicationBuild extends Build {
+  import Resolvers._
+  import Dependencies._
+  import Versions._
+  
+  lazy val appName         = "play-eventsourced"
+  lazy val appVersion      = "1.0-SNAPSHOT"
+
+  lazy val appDependencies = Seq(jdbc, anorm) ++ basicDependencies
+
+  lazy val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here
+    scalaVersion := scalaVer,
+    resolvers ++= allResolvers
+  )
+
+}
